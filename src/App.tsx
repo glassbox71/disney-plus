25d6b73233ae5b382234e0aba7fb29d38a8b14d2
kwon hana <kwon hana>
2025-12-08 12:38:36 +0900

    init : header / main router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './pages/Header/Header';
+import MainPage from './pages/Main/MainPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
