f906677818305825a9d429f56dbb76cfdd811bcf
kwon hana <kwon hana>
2025-12-12 16:18:22 +0900

    fix : 장르 코드 위치 변경

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -6,11 +6,31 @@ import ThemeList from './components/ThemeList';
 import UpcomingList from './components/UpcomingList';
 import GenreList from './components/GenreList';
 
+// 12: "모험"
+// 14: "판타지"
+// 16: "애니메이션"
+// 18: "드라마"
+// 27: "공포"
+// 28: "액션"
+// 35: "코미디"
+// 36: "역사"
+// 37: "서부"
+// 53: "스릴러"
+// 80: "범죄"
+// 99: "다큐멘터리"
+// 878: "SF"
+// 9648: "미스터리"
+// 10402: "음악"
+// 10749: "로맨스"
+// 10751: "가족"
+// 10752: "전쟁"
+// 10770: "TV 영화"
 const MainPage = () => {
   return (
     <section className="MainPage normal">
       <MainScreen />
       <WatchList />
+      <GenreList genreId="27" title="공포" />
       <RecommendedForYou />
       <ThemeList />
       <UpcomingList />
