438ff2f62e19e5ed3095b98d095c76c167f88ded
kwon hana <kwon hana>
2025-12-16 13:03:25 +0900

    init : Top10 추가

--- src/pages/Main/components/Top10List.tsx
+++ src/pages/Main/components/Top10List.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import '../scss/Top10List.scss';
+import { useEffect } from 'react';
+import { useMovieStore } from '../../../store/useMovieStore';
+import HeaderTitle from './HeaderTitle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Link } from 'react-router-dom';
 
 //TODO 탑10 리스트
-const Top10List = () => {
-  return <div>Top10List</div>;
+
+interface Top10Props {
+  title: string | number;
+}
+const Top10List = ({ title }: Top10Props) => {
+  const { onFetchTOP, Top } = useMovieStore();
+
+  useEffect(() => {
+    if (Top.length === 0) {
+      onFetchTOP();
+    }
+  }, [Top, onFetchTOP]);
+
+  console.log('Top', Top);
+  return (
+    <section className="Top10List">
+      <HeaderTitle mainTitle={title} />
+      <Swiper slidesPerView={3.4} spaceBetween={20} className="mySwiper">
+        {Top.slice(0, 10).map((el, i) => {
+          return (
+            <SwiperSlide>
+              <Link to="void">
+                <div className={`movieThumbnail TopNumber number${1 + i}`}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${el.poster_path}`}
+                    alt={`${el.title} 썸네일`}
+                  />
+                </div>
+              </Link>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Top10List;
