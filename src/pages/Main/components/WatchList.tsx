3e15499e01d14bf954a2e6bc59f91c699e23b27a
yuleehana <yuleehana@gmail.com>
2025-12-18 18:18:25 +0900

    feat: 키즈모드 & 로컬 스토리지 설정

--- src/pages/Main/components/WatchList.tsx
+++ src/pages/Main/components/WatchList.tsx
@@ -5,13 +5,20 @@ import { Pagination } from 'swiper/modules';
 import { NowWatchListData } from '../../../store/data';
 import HeaderTitle from './HeaderTitle';
 import { Link } from 'react-router-dom';
+import { useProfileStore } from '../../../store/useProfileStore';
 
 //TODO 현재 시청 중인 콘텐츠
 // ? 로그인 전에는 노출 불가
 const WatchList = () => {
+  const { profiles, activeProfileId } = useProfileStore();
+
+  const activeProfile = profiles.find((profile) => profile.id === activeProfileId);
+
   return (
     <section className="WatchList movieList pullInner marginUp">
-      <HeaderTitle mainTitle="@@@님이 시청중인 콘텐츠" />
+      <HeaderTitle
+        mainTitle={activeProfile ? `${activeProfile.name}님이 시청중인 콘텐츠` : '시청중인 콘텐츠'}
+      />
       <>
         <Swiper
           slidesPerView={4.3}
